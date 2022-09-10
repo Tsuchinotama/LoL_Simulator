c8579437ca9c8db9a16866e1eb10afbd46deb1be
Tsuchinotama <bruno.puissant63@gmail.com>
2022-09-10 07:27:41 +0200

    Add compatibility booleans

--- Worlds_2022_groups_generator/generate_groups.py
+++ Worlds_2022_groups_generator/generate_groups.py
@@ -1,5 +1,7 @@
+import copy
 from itertools import permutations
 import collections
+import itertools
 
 all_main_teams = ["JDG", "TES", "EDG", "GENG", "T1", "DWG", "G2", "RGE", "C9", "100T", "CTO", "GAM"] 
 
@@ -13,9 +15,6 @@ team_regions = {"TES" : "CHN", "JDG" : "CHN", "EDG" : "CHN", "RNG" : "CHN", "G2"
 list_all_groups_main = []
 
 occurence_all_groups_main = [[] for i in range(4)]
-
-print({frozenset(["a", "b"]), frozenset(["c", "d"])} == {frozenset(["b", "a"]), frozenset(["d", "c"])})
-print({frozenset(["a", "b"]), frozenset(["c", "d"])} in [{frozenset(["b", "a"]), frozenset(["d", "c"])}])
 
 set_all_groups = set()
 first_pool_teams = ["JDG", "GENG", "G2", "C9"]
@@ -44,7 +43,71 @@ for place_second_pool in list_places_second_pool:
 
                 one_conf_groups = frozenset(tuple(group) for group in new_init_first_pool_teams)
                 set_all_groups.add(one_conf_groups)
-            
-print(set_all_groups)
+
+# print(list(set_all_groups))
+# for all_groups in set_all_groups:            
+#     print(all_groups)
+#     print(list(all_groups))
 print(len(set_all_groups))
+list_set_all_groups = list(set_all_groups)
+
+no_wildcard_play_in_teams = ["RNG", "DRX", "FNC", "BYG", "MAD", "EG", "SAI"]
+
+def check_valid_affect(qualif_play_in, list_main_groups):
+    if not ("FNC" in qualif_play_in) or not ("MAD" in qualif_play_in):
+        for order_qualif in itertools.permutations(qualif_play_in):
+            for i in range(4):
+                to_affect_team = order_qualif[i]
+                current_group= list_main_groups[i]
+                for qualified in current_group:
+                    if team_regions[qualified] == team_regions[to_affect_team]:
+                        break
+                else:
+                    continue
+                break
+            else:
+                return (True, None)
+        else:
+            return (False, qualif_play_in)
+
+    else:
+        for order_qualif in itertools.permutations(qualif_play_in):
+            for i in range(4):
+                to_affect_team = order_qualif[i]
+                current_group= list_main_groups[i]
+                if to_affect_team == "MAD":
+                    continue
+                for qualified in current_group:
+                    if team_regions[qualified] == team_regions[to_affect_team]:
+                        break
+                else:
+                    continue
+                break
+            else:
+                return (True, None)
+        else:
+            return (False, qualif_play_in)
+
+
+# copy_set_all_groups = list(set_all_groups)
+copy_set_all_groups = set_all_groups.copy()
+set_groups_with_compatibility = set((tirage, True) for tirage in list_set_all_groups)
+print(set_groups_with_compatibility)
+list_set_groups_with_compatibility = list(set_groups_with_compatibility)
+for i in range(len(set_all_groups)):
+    main_groups = list_set_all_groups[i]
+# for main_groups in set_all_groups:
+    for qualif_from_play_in in itertools.combinations(no_wildcard_play_in_teams, 4):
+        if not check_valid_affect(qualif_from_play_in, list(main_groups))[0]:
+            copy_set_all_groups.remove(main_groups)
+            list_set_groups_with_compatibility[i] = (main_groups, False)
+            break
+
+# print(set_all_groups)
+print(len(copy_set_all_groups))
+
+print(set_all_groups.difference(copy_set_all_groups))
+print()
+print(list_set_groups_with_compatibility)
+    
 
